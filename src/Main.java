ecc7dd409c7ce2a0701cc59b3444c691415562a3
Pedro Fontes <pedro7fontes@gmail.com>
2023-03-28 11:28:31 -0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI address = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(address).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        var parser = new JsonParser();
+        List<Map<String, String>> moviesList = parser.parse(body);
+
+        for (Map<String, String> movie : moviesList
+             ) {
+            System.out.println(movie.get("title"));
+            System.out.println(movie.get("image"));
+            System.out.println(movie.get("imDbRating"));
+            System.out.println();
+
+        }
+
+    }
+}
